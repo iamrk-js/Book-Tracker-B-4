48f6c197b1d89c4c2ca67c8bcab025cfb8465f8f
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2022-12-15 02:26:54 +0530

    set up server

---
+++ src/app/data.ts
@@ -0,0 +1,68 @@
+import { Book } from "app/models/book";
+import { Reader } from "app/models/reader";
+
+export const allReaders: Reader[] = [
+  {
+    "readerID": 1,
+    "name": "Ram",
+    "weeklyReadingGoal": 350,
+    "totalMinutesRead": 5600
+  },
+  {
+    "readerID": 2,
+    "name": "Sham",
+    "weeklyReadingGoal": 210,
+    "totalMinutesRead": 3000
+  },
+  {
+    "readerID": 3,
+    "name": "Sita",
+    "weeklyReadingGoal": 140,
+    "totalMinutesRead": 600
+  }
+  ,
+  {
+    "readerID": 4,
+    "name": "Gita",
+    "weeklyReadingGoal": 280,
+    "totalMinutesRead": 1200
+  }
+];
+
+export const allBooks: Book[] = [
+  {
+    "bookID": 1,
+    "title": "Rich Dad Poor Dad",
+    "author": "Robert  Kiyosaki",
+    "publicationYear": 1997
+  },
+  {
+    "bookID": 2,
+    "title": "Asa Mi Asami",
+    "author": "P. L. Deshpande",
+    "publicationYear": 1964
+  },
+  {
+    "bookID": 3,
+    "title": "Wings of fire",
+    "author": "A. P. J. Abdul Kalam",
+    "publicationYear": 1999
+  },
+  {
+    "bookID": 4,
+    "title": "A Monck How sold his Farrari",
+    "author": "Rabin Sharma",
+    "publicationYear": 1996
+  },
+  {
+    "bookID": 5,
+    "title": "The Power of your subconscious mind",
+    "author": " Joseph Murphy",
+    "publicationYear": 1963
+  },
+  {
+    "bookID": 6,
+    "title": "Yayati",
+    "author": " V. S. Khandekar",
+    "publicationYear": 1959
+  }];
